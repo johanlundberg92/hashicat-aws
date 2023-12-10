b9fc26835d7bc2fa1bb64ea7a01d9149c2aa5390
johanlundberg92 <johan.lundberg1992@gmail.com>
2023-12-10 15:35:42 +0000

    Added s3 buckets module

---
+++ s3-bucket.tf
@@ -0,0 +1,15 @@
+module "s3-bucket" {
+  source  = "app.terraform.io/jlab/s3-bucket/aws"
+  version = "2.8.0"
+}
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+  bucket_prefix = "j-bucket-"
+  bucket = "my-s3-bucket"
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+
+}
